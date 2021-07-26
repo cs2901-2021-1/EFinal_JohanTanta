167c6bed20c943870c033d71c7ab851f4ba2b885
JohanTv <johan.tanta@utec.edu.pe>
2021-07-26 14:27:42 -0500

    initial commit

---
+++ src/main/java/cs/lab/Demo.java
@@ -0,0 +1,12 @@
+package cs.lab;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Demo {
+    static final Logger logger = Logger.getLogger(Demo.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        logger.info("PC Template");
+    }
+}
---
+++ src/test/java/cs/lab/DemoTester.java
@@ -0,0 +1,9 @@
+package cs.lab;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.logging.Logger;
+
+class DemoTester {
+    static final Logger logger = Logger.getLogger(DemoTester.class.getName());
+};
